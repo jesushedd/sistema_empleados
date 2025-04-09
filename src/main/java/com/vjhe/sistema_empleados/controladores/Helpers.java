ba2d2cde3658ee0d7970f9b5d543bae4c42bfa3d
jesushedd <jesushe99@gmail.com>
2025-04-09 05:59:06 -0500

    Add helpers for fields validations

---
+++ src/main/java/com/vjhe/sistema_empleados/controladores/Helpers.java
@@ -0,0 +1,24 @@
+package com.vjhe.sistema_empleados.controladores;
+
+import javafx.scene.control.TextField;
+
+public class Helpers {
+    public static void restringirEntradaNumerica(TextField campo){
+        campo.textProperty().addListener((observable, oldValue, newValue) -> {
+            /*if (newValue.matches("\\d*[\\d.]?\\d*")){
+                return;
+            } else {
+                campo.setText(oldValue);
+            }*/
+            if (!newValue.matches("\\d*[\\d.]?\\d*")){
+                campo.setText(oldValue);
+            }
+        });
+    }
+
+    public static void restringirEntradaAlfabetica(TextField campo){
+        campo.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("^[\\p{L} '-]*$")) campo.setText(oldValue);
+        });
+    }
+}
